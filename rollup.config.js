f62a5d5348f4b8f4c837bcc0ec6e2680396a98df
Gustaf <gustaf.j.dahl@gmail.com>
2024-07-24 00:22:49 +0200

    trnsf

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,34 @@
-export default {
-  input: ['dist/managers/index.js'],
-  output: {
-    dir: 'dist',
-    format: 'cjs',
-    entryFileNames: '[name].cjs',
-    preserveModules: true,
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+// const packageJson = require('./package.json');
+
+export default [
+  {
+    input: ['src/index.ts', 'src/managers/index.ts'],
+    output: [
+      {
+        file: './src/index.ts',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: './src/index.ts',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
   },
-};
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
