461c5bc45919c3739757542c3fee10345ed78229
Gustaf Dahl <gustaf-dahl@telia.com>
2024-07-24 16:25:53 +0200

    working

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const Config = {
-  BB_DEBUG: false,
-  BB_API_URL: 'https://external.api.byteboost.io/v1beta/public',
+  BB_DEBUG: true,
+  BB_API_URL: 'https://dev.external.api.byteboost.io/v1beta/public',
 };
