e247181882494474a2d45f4932051ba830ef0759
Gustaf Dahl <gustaf-dahl@telia.com>
2025-01-04 12:20:07 +0100

    backup

--- src/managers/sourcemaps/upload.ts
+++ src/managers/sourcemaps/upload.ts
@@ -23,7 +23,8 @@ enum FileType {
 }
 
 interface EnvConf extends dotenv.DotenvParseOutput {
-  BYTEBOOST_TOKEN: string;
+  BYTEBOOST_PRIVATE_KEY: string;
+  BYTEBOOST_PUBLIC_KEY: string;
   BYTEBOOST_DOMAIN: string;
   BYTEBOOST_ORGANIZATION: string;
 }
@@ -34,13 +35,97 @@ export class UploadSourceMapsHandler {
 
   env: Partial<EnvConf> = {};
 
-  public version: string | null = null;
+  public sessionId: string | null = null;
 
   constructor(
     public path: string,
     public distName?: string,
   ) {
     this.fullpath = join(path, distName ?? '');
+  }
+
+  public async startUploadSession(version?: string) {
+    try {
+      const res = await fetch(
+        `${Config.BB_API_URL}/sourcemaps/session${version ? `?version=${version}` : ''}`,
+        {
+          method: 'GET',
+          headers: {
+            ['X-Auth-Token']: this.env.BYTEBOOST_PRIVATE_KEY!,
+            ['X-Identifier-Token']: this.env.BYTEBOOST_PUBLIC_KEY!,
+            ['X-Domain']: this.env.BYTEBOOST_DOMAIN!,
+            ['X-Organization']: this.env.BYTEBOOST_ORGANIZATION!,
+          },
+          insecureHTTPParser: true,
+        },
+      );
+
+      if (res.status === 404) {
+        throw new Error('404 not found');
+      }
+
+      const data = (await res.json()) as {
+        data: string;
+        errors: { message: string }[];
+      };
+
+      if (res.status !== 201) {
+        if (!data.errors[0]) {
+          throw new Error(
+            'Unexpected error. please contact us at support@byteboost.io',
+          );
+        }
+
+        throw new Error(data.errors[0].message);
+      }
+
+      return data.data;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async finishSession() {
+    try {
+      if (!this.sessionId) {
+        throw new Error('No upload session found');
+      }
+
+      const res = await fetch(`${Config.BB_API_URL}/sourcemaps/session`, {
+        method: 'PUT',
+        headers: {
+          ['X-Session']: this.sessionId!,
+          ['X-Auth-Token']: this.env.BYTEBOOST_PRIVATE_KEY!,
+          ['X-Identifier-Token']: this.env.BYTEBOOST_PUBLIC_KEY!,
+          ['X-Domain']: this.env.BYTEBOOST_DOMAIN!,
+          ['X-Organization']: this.env.BYTEBOOST_ORGANIZATION!,
+        },
+        insecureHTTPParser: true,
+      });
+
+      if (res.status === 404) {
+        throw new Error('404 not found');
+      }
+
+      const data = (await res.json()) as {
+        data: string;
+        errors: { message: string }[];
+      };
+
+      if (res.status !== 201) {
+        if (!data.errors[0]) {
+          throw new Error(
+            'Unexpected error. please contact us at support@byteboost.io',
+          );
+        }
+
+        throw new Error(data.errors[0].message);
+      }
+
+      return data.data;
+    } catch (err) {
+      throw err;
+    }
   }
 
   public isPathValidJsDirectory() {
@@ -70,7 +155,7 @@ export class UploadSourceMapsHandler {
 
         const mimetype = mime.getType(join(path, item));
 
-        if (mimetype === FileType.ApplicationJson && item.includes('.map')) {
+        if (mimetype === FileType.ApplicationJson && item.includes('.js.map')) {
           this.mapFilePaths.push(join(path, item));
 
           continue;
@@ -118,10 +203,14 @@ export class UploadSourceMapsHandler {
   }
 
   public async tagFilesWithDebugInfo() {
-    for (const path of this.mapFilePaths) {
-      const sourceCodePath = path.replace('.map', '');
+    if (this.mapFilePaths.length === 0) {
+      throw new Error('No sourcemaps found to tag');
+    }
 
-      const fileGroup = [sourceCodePath, path];
+    for (const sourceMapPath of this.mapFilePaths) {
+      const minifiedCodePath = sourceMapPath.replace('.map', '');
+
+      const fileGroup = [minifiedCodePath, sourceMapPath];
 
       const debugId = nanoid();
       for (const filePath of fileGroup) {
@@ -141,7 +230,7 @@ export class UploadSourceMapsHandler {
           );
 
           if (sourceMapContent) {
-            sourceMapContent.debugId = debugId;
+            sourceMapContent.bbDebugId = debugId;
 
             writeFileSync(filePath, JSON.stringify(sourceMapContent));
           }
@@ -166,18 +255,15 @@ export class UploadSourceMapsHandler {
       });
     }
 
-    if (this.version) {
-      form.append('version', this.version);
-    }
-
-    form.append('organization', this.env.BYTEBOOST_ORGANIZATION);
-    form.append('domain', this.env.BYTEBOOST_DOMAIN);
-
-    return fetch(`${Config.BB_API_URL}/sourcemaps`, {
+    return fetch(`${Config.BB_API_URL}/sourcemaps/upload`, {
       method: 'POST',
       headers: {
         ...form.getHeaders(),
-        ['X-Auth-Token']: this.env.BYTEBOOST_TOKEN!,
+        ['X-Session']: this.sessionId!,
+        ['X-Domain']: this.env.BYTEBOOST_DOMAIN!,
+        ['X-Organization']: this.env.BYTEBOOST_ORGANIZATION!,
+        ['X-Auth-Token']: this.env.BYTEBOOST_PRIVATE_KEY!,
+        ['X-Identifier-Token']: this.env.BYTEBOOST_PUBLIC_KEY!,
       },
       insecureHTTPParser: true,
       body: form,
@@ -196,7 +282,19 @@ export class UploadSourceMapsHandler {
       return;
     }
 
-    const res = await this.uploadFiles(this.mapFilePaths);
+    const paths: string[] = [];
+
+    for (const path of this.mapFilePaths) {
+      if (path.endsWith('.map')) {
+        const splitPath = path.split('.map');
+
+        paths.push(splitPath.join(''));
+      }
+
+      paths.push(path);
+    }
+
+    const res = await this.uploadFiles(paths);
 
     if (res.status !== 201) {
       console.log('[Byteboost] Failed to upload sourcemaps');
@@ -214,8 +312,12 @@ export class UploadSourceMapsHandler {
       const buf = Buffer.from(content);
       const config = dotenv.parse<EnvConf>(buf);
 
-      if (!config.BYTEBOOST_TOKEN) {
-        return 'BYTEBOOST_TOKEN is required';
+      if (!config.BYTEBOOST_PRIVATE_KEY) {
+        return 'BYTEBOOST_PRIVATE_KEY is required';
+      }
+
+      if (!config.BYTEBOOST_PUBLIC_KEY) {
+        return 'BYTEBOOST_PUBLIC_KEY is required';
       }
 
       if (!config.BYTEBOOST_DOMAIN) {
@@ -226,8 +328,8 @@ export class UploadSourceMapsHandler {
         return 'BYTEBOOST_ORGANIZATION is required';
       }
 
-      if (!validate(config.BYTEBOOST_TOKEN)) {
-        return 'Invalid auth token';
+      if (!validate(config.BYTEBOOST_PUBLIC_KEY)) {
+        return 'Invalid identifier token. Must be an uuid';
       }
 
       this.env = config;
